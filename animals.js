07c2b36efb6ace1a0822e24f2661d58c4d997e12
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-27 08:35:31 -0300

    cp2-web-front

---
+++ animals.js
@@ -0,0 +1,7 @@
+const animals = ["leão", "tigre", "lemure", "alpaca", "bagre"]
+
+if (animals.includes('leão')){
+    console.log("Existe um leão no array")
+} else {
+    console.log("Leão não existe no array")
+}
---
+++ series.js
@@ -0,0 +1,5 @@
+const my3FavoriteTVShows = ["Ben 10", "Gossip Girl", "Barbie"];
+
+if (my3FavoriteTVShows.length == 3){
+    console.log("Possui 3 itens!")
+}
